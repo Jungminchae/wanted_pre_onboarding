23c154e507eb50294184ff9948512135793f3493
Jungminchae <minchae3618@gmail.com>
2022-08-16 10:03:35 +0900

    Fix: typo in test code

--- app/apis/tests/test_models.py
+++ app/apis/tests/test_models.py
@@ -1,4 +1,4 @@
-import pytest 
+import pytest
 from apis.models import User, Company, JobPost, ApplyList
 
 
@@ -6,55 +6,50 @@ pytestmark = pytest.mark.django_db
 
 
 def test_user_model_success():
-    '''
+    """
     유저 모델 테스트 - 성공
-    '''    
+    """
     user = User.objects.create(
-        name='test123',
-        email="test123@example.com"
+        name="test123",
+        email="test123@example.com",
         is_company=True,
     )
-    assert user.name == 'test123'
+    assert user.name == "test123"
     assert user.email == "test123@example.com"
 
+
 def test_company_model_success():
-    '''
+    """
     회사 모델 테스트 - 성공
-    '''    
-    company = Company.objects.create(
-        name='원티드',
-        country='한국',
-        region='서울'
-    )
-    assert company.name == '원티드'
-    assert company.country == '한국'
-    assert company.region == '서울'
+    """
+    company = Company.objects.create(name="원티드", country="한국", region="서울")
+    assert company.name == "원티드"
+    assert company.country == "한국"
+    assert company.region == "서울"
+
 
 def test_jobpost_model_success(sample_user, sample_company):
-    '''
+    """
     채용공고 모델 테스트 - 성공
-    '''    
+    """
     jobpost = JobPost.objects.create(
-        title='백엔드 엔지니어 채용공고',
-        content='백엔드 엔지니어 채용합니다',
-        position='백엔드 엔지니어',
+        title="백엔드 엔지니어 채용공고",
+        content="백엔드 엔지니어 채용합니다",
+        position="백엔드 엔지니어",
         compensation=1000000,
-        skill='python',
+        skill="python",
         company=sample_company,
-        user=sample_user
+        user=sample_user,
     )
-    assert jobpost.title == '백엔드 엔지니어 채용공고'
-    assert jobpost.content == '백엔드 엔지니어 채용합니다'
+    assert jobpost.title == "백엔드 엔지니어 채용공고"
+    assert jobpost.content == "백엔드 엔지니어 채용합니다"
     assert jobpost.user == sample_user
 
 
 def test_applylist_model_success(sample_user, sample_jobpost):
-    '''
+    """
     지원 내역 모델 테스트 - 성공
-    '''    
-    applylist = ApplyList.objects.create(
-        user=sample_user,
-        job_post=sample_jobpost
-    )
+    """
+    applylist = ApplyList.objects.create(user=sample_user, job_post=sample_jobpost)
     assert applylist.user == sample_user
     assert applylist.job_post == sample_jobpost
--- app/conftest.py
+++ app/conftest.py
@@ -6,28 +6,27 @@ from apis.models import User, Company, JobPost
 @pytest.fixture
 def sample_user():
     return User.objects.create(
-        name='test123',
-        email='test123@example.com'
+        name="test123",
+        email="test123@example.com",
+        is_company=False,
     )
+
 
 @pytest.fixture
 def sample_company():
-    return Company.objects.create(
-        name='원티드',
-        country='한국',
-        region='서울'
-    )
+    return Company.objects.create(name="원티드", country="한국", region="서울")
+
 
 @pytest.fixture
 def sample_jobpost():
     company = mixer.blend(Company)
     user = mixer.blend(User)
     return JobPost.objects.create(
-        title='백엔드 엔지니어 채용공고',
-        content='백엔드 엔지니어 채용합니다',
-        position='백엔드 엔지니어',
+        title="백엔드 엔지니어 채용공고",
+        content="백엔드 엔지니어 채용합니다",
+        position="백엔드 엔지니어",
         compensation=1000000,
-        skill='python',
+        skill="python",
         company=company,
-        user=user
+        user=user,
     )

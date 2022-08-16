ba2468812e4f11872d8e0a8249d007fba0466851
Jungminchae <minchae3618@gmail.com>
2022-08-17 00:25:29 +0900

    Refactor: jobpost validation when trying to fix company id

--- app/apis/serializers/jobpost_serializers.py
+++ app/apis/serializers/jobpost_serializers.py
@@ -14,3 +14,11 @@ class JobPostSerializer(serializers.ModelSerializer):
             "user",
             "company",
         )
+
+
+class JobPostUpdateSerializer(JobPostSerializer):
+    def validate(self, attrs):
+        if "company" in attrs:
+            if attrs["company"] != self.instance.company:
+                raise serializers.ValidationError("채용공고의 회사 ID를 변경할 수 없음")
+        return super().validate(attrs)
--- app/apis/views/jobpost_views.py
+++ app/apis/views/jobpost_views.py
@@ -1,13 +1,25 @@
 from rest_framework.generics import CreateAPIView, UpdateAPIView
 from apis.models import JobPost
-from apis.serializers.jobpost_serializers import JobPostSerializer
+from apis.serializers.jobpost_serializers import (
+    JobPostSerializer,
+    JobPostUpdateSerializer,
+)
 
 
 class JobPostRegistrationView(CreateAPIView):
+    """
+    채용공고 등록
+    """
+
     serializer_class = JobPostSerializer
     queryset = JobPost.objects.all()
 
 
 class JobPostUpdateView(UpdateAPIView):
-    serializer_class = JobPostSerializer
+    """
+    채용공고 수정
+    :Company ID 제외, 전부 변경 가능
+    """
+
+    serializer_class = JobPostUpdateSerializer
     queryset = JobPost.objects.all()
